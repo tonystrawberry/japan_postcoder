d2c50c424feea1f21579001b4b55434b0b7bf0e9
Tony Duong <tony.duong.102@gmail.com>
2023-08-22 23:30:55 +0900

    :bug: fix rubocop alerts

--- lib/japan_postcoder.rb
+++ lib/japan_postcoder.rb
@@ -38,8 +38,6 @@ module JapanPostcoder
     address.to_h
   end
 
-  private
-
   # Validates the postal code format
   # Raises InvalidPostalCodeError if the postal code is invalid
   def self.validate_postal_code!(postal_code)
@@ -51,7 +49,11 @@ module JapanPostcoder
 
   # Returns an Address object
   # Example:
-  # #<Japan::Address:0x00007f9d0a8d3d88 @postcode="2140037", @prefecture="神奈川県", @city="川崎市", @ward="多摩区", @district="西生田">
+  # #<Japan::Address:0x00007f9d0a8d3d88 @postcode="2140037",
+  #   @prefecture="神奈川県",
+  #   @city="川崎市",
+  #   @ward="多摩区",
+  #   @district="西生田">
   def self.get_address(postal_code, romaji: false)
     # Read from ./jp_postcodes.json (Shift JIS)
     # Format: {
--- lib/japan_postcoder/address.rb
+++ lib/japan_postcoder/address.rb
@@ -1,37 +1,39 @@
 # frozen_string_literal: true
 
-## Class for Japanese Address
-## Has attributes for postcode, prefecture, city, ward and district
-#
-class JapanPostcoder::Address
-  attr_accessor :postcode, :prefecture, :city, :ward, :district
+module JapanPostcoder
+  ## Class for Japanese Address
+  ## Has attributes for postcode, prefecture, city, ward and district
+  #
+  class Address
+    attr_accessor :postcode, :prefecture, :city, :ward, :district
 
-  # Initialize the address with postcode, prefecture, city, ward and district
-  def initialize(postcode, prefecture, city, ward, district)
-    @postcode = postcode
-    @prefecture = prefecture
-    @city = city
-    @ward = ward
-    @district = district
-  end
-
-  # Returns the full address as a string
-  def to_s(romaji: false)
-    if romaji
-      "#{@prefecture} #{@city} #{@ward} #{@district}"
-    else
-      "#{@prefecture}#{@city}#{@ward}#{@district}"
+    # Initialize the address with postcode, prefecture, city, ward and district
+    def initialize(postcode, prefecture, city, ward, district)
+      @postcode = postcode
+      @prefecture = prefecture
+      @city = city
+      @ward = ward
+      @district = district
     end
-  end
 
-  # Returns the full address as a hash
-  def to_h
-    {
-      postcode: @postcode,
-      prefecture: @prefecture,
-      city: @city,
-      ward: @ward,
-      district: @district
-    }
+    # Returns the full address as a string
+    def to_s(romaji: false)
+      if romaji
+        "#{@prefecture} #{@city} #{@ward} #{@district}"
+      else
+        "#{@prefecture}#{@city}#{@ward}#{@district}"
+      end
+    end
+
+    # Returns the full address as a hash
+    def to_h
+      {
+        postcode: @postcode,
+        prefecture: @prefecture,
+        city: @city,
+        ward: @ward,
+        district: @district
+      }
+    end
   end
 end
