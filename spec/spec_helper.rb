7a42fa694844e6eed6dbfe954dca41ebb98a5190
Tony Duong <tony.duong.102@gmail.com>
2023-08-23 00:32:56 +0900

    :bug: fix main.yml

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,6 @@ SimpleCov.start
 
 require 'japan_postcoder'
 
-
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
